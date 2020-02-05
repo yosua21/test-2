9743ab82cd44a206f6838d112e2fd97bc3cc2560
yosua21 <yosuasendy29@gmail.com>
2020-02-05 10:19:29 +0700

    menambahkan pragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>coba git </title>
+</head>
+<body>
+    <h1>p</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quisquam soluta, non praesentium facere quo doloribus eligendi tenetur quam quia quod voluptates modi ipsum quas possimus vitae! Cumque, quisquam quod.</p>
+</body>
+</html>
